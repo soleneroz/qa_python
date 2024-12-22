73ac97723e6a0f43535fa5cc66ee8ca530759dd5
Nikita Kopyev <gorduxa69@yandex.ru>
2024-12-22 17:22:11 +0500

    Коммит

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from main import BooksCollector
+
+@pytest.fixture
+def my_book_collection(book_collection):
+    collection = book_collection
+    books = ['100 лет одиночества', 'Ведьмак', 'Капоте', 'Джедайские техники', 'Оно']
+    genre = ['Драма', 'Ужасы', 'Фантастика', 'Обучающее', 'Детектив']
+    for i in range(5):
+        collection.add_new_book(books[i])
+    for i in range(5):
+        collection.set_book_genre(books[i], genre[i])
+    return collection
+
+
+@pytest.fixture
+def book_collection():
+    book_collection = BooksCollector()
+    return book_collection
